a188c30210f5ab313726b8fba84e80af72040f03
bigby347 <alexandreputzu@gmail.com>
2018-02-06 01:06:10 +0100

    ajout bdd

---
+++ site_web/bdd/Base.sql
@@ -0,0 +1,174 @@
+Create table Oeuvre (
+	IdLivre integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	Nom varchar(100)
+	NOT NULL,
+	Cote varchar(100),
+	Publication date
+); 
+
+Create table Auteur (
+	IdAuteur integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	Nom varchar(100)
+	NOT NULL,
+	Prenom varchar(100)
+);
+
+Create table Ecrit (
+	FkAuteur integer
+	NOT NULL,
+	FkLivre integer
+	NOT NULL,
+	PRIMARY KEY(FkAuteur, FkLivre),
+	FOREIGN KEY (FkAuteur) REFERENCES Auteur(IdAuteur) ON UPDATE CASCADE,
+	FOREIGN KEY (FkLivre) REFERENCES Oeuvre(IdLivre) ON UPDATE CASCADE
+);
+
+Create table MotClef (
+	IdMotClef integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	Nom varchar(100)
+	NOT NULL
+);
+
+Create table Definition (
+	FkMotClef integer
+	NOT NULL,
+	FkLivre integer
+	NOT NULL,
+	PRIMARY KEY(FkMotClef, FkLivre),
+	FOREIGN KEY (FkMotClef) REFERENCES MotClef(IdMotClef) ON UPDATE CASCADE,
+	FOREIGN KEY (FkLivre) REFERENCES Oeuvre(IdLivre) ON UPDATE CASCADE
+);
+
+
+Create table Livre (
+	IdExemplaire integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	FkLivre integer
+	NOT NULL, 
+	Achat date,
+	FOREIGN KEY (FkLivre) REFERENCES Oeuvre(IdLivre) ON UPDATE CASCADE
+);
+
+
+Create table Adherant (
+	IdAdherant integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	MDP varchar(100)
+	NOT NULL,
+	Nom varchar(100)
+	NOT NULL,
+	Prenom varchar(100)
+	NOT NULL,
+	Adresse varchar(100)
+	NOT NULL,
+	adhesion date,
+	cotisation date
+);
+
+Create table Admin (
+	IdAdmin integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	MDP varchar(100)
+	NOT NULL,
+	Nom varchar(100)
+	NOT NULL,
+	Prenom varchar(100)
+	NOT NULL,
+	Adresse varchar(100)
+	NOT NULL
+);
+
+
+Create table TypeNotif (
+	IdTypeNotif integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	Nom varchar(100)
+	NOT NULL
+);
+
+Create table Notif (
+	IdNotif integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	FkAdherant integer
+	NOT NULL,
+	FkTypeNotif integer
+	NOT NULL,
+	FOREIGN KEY (FkTypeNotif) REFERENCES TypeNotif(IdTypeNotif) ON UPDATE CASCADE,
+	FOREIGN KEY (FkAdherant) REFERENCES Adherant(IdAdherant) ON UPDATE CASCADE
+);
+
+Create table Emprun (
+	IdEmprun integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	FkAdherant integer
+	NOT NULL,
+	FkExemplaire integer
+	NOT NULL,
+	DatePret date NOT NULL,
+	DateRetour date,
+	Renouvelement integer,
+	FOREIGN KEY (FkAdherant) REFERENCES Adherant(IdAdherant) ON UPDATE CASCADE,
+	FOREIGN KEY (FkExemplaire) REFERENCES Livre(IdExemplaire) ON UPDATE CASCADE
+);
+
+
+Create table Renouvelement(
+	FkEmprun integer,
+	DateDemande date,
+	FOREIGN KEY (FkEmprun) REFERENCES Emprun(IdEmprun) ON UPDATE CASCADE
+);
+
+
+
+
+Create table Requete (
+	IdRequete integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	FkAdherant integer
+	NOT NULL,
+	FkLivre integer
+	NOT NULL,
+	Requete date NOT NULL,
+	FOREIGN KEY (FkAdherant) REFERENCES Adherant(IdAdherant) ON UPDATE CASCADE,
+	FOREIGN KEY (FkLivre) REFERENCES Oeuvre(IdLivre) ON UPDATE CASCADE
+);
+
+
+Create table Reservation (
+	IdReservation integer
+	PRIMARY KEY
+	AUTO_INCREMENT,
+	FkAdherant integer
+	NOT NULL,
+	FkLivre integer
+	NOT NULL,
+	DateRequete date NOT NULL,
+	DateAcceptation date NOT NULL,
+	FOREIGN KEY (FkAdherant) REFERENCES Adherant(IdAdherant) ON UPDATE CASCADE,
+	FOREIGN KEY (FkLivre) REFERENCES Oeuvre(IdLivre) ON UPDATE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
