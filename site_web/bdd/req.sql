d8c04992ab3c255f9804083c5d39a86dba19a3d2
FALQUES Theo <f17013546@c-sc-ch205-dosi>
2018-02-15 14:25:31 +0100

    Ajoue de requete adherant

--- site_web/bdd/req.sql
+++ site_web/bdd/req.sql
@@ -1,24 +1,26 @@
 /* Adherant */
-/* Catalogue */ 
 
+/* Catalogue */ 
 Select Oeuvre.IdLivre, Oeuvre.Nom, Oeuvre.Cote, Year(Oeuvre.Publication), group_concat(Auteur.Nom, Auteur.Prenom , ','),  group_concat(MotClef.Nom, ',')
 From Oeuvre, Ecrit, Auteur , Definition, Motclef
 Where Oeuvre.IdLivre = Ecrit.FkLivre
 And Ecrit.FkAuteur = Auteur.IdAuteur 
 And Definition.FkLivre = Oeuvre.IdLivre
 And Definition.FkMotClef = MotClef.IdMotClef 
-And Auteur.IdAuteur = ???
-And MotClef.IdMotClef = ????
-LIMIT 20 OFSET 0;
-
+And Auteur.IdAuteur = '???'
+And MotClef.IdMotClef = '???'
+LIMIT 20 OFSET '???' ;
+/*	*/
 
 
 /* Notif */ 
 Select IdNotif, TypeNotif.Nom as Type, Notif.Commentaire
 From Notif, TypeNotif , Adherant 
-where IdAdherant = ???
+where IdAdherant = '???'
 AND IdAdherant = Notif.FkAdherant
 AND IdTypeNotif = Notif.FkTypeNotif; 
+/*	*/
+
 
 /* Historique */ 
 Select * 
@@ -26,8 +28,10 @@ From Emprun, Exemplaire, Oeuvre, Adherant
 Where Oeuvre.IdLivre = Exemplaire.FkLivre
 And Livre.IdExemplaire = Emprun.FkExemplaire
 And Emprun.FkAdherant = Adherant.IdAdherant
-And Adherant.IdAdherant = ???
+And Adherant.IdAdherant = '???'
 And Emprun.DateRetour IS NOT NULL;
+/*	*/
+
 
 /* Pret en cour */
 Select * 
@@ -35,24 +39,57 @@ From Emprun, Exemplaire, Oeuvre, Adherant
 Where Oeuvre.IdLivre = Exemplaire.FkLivre
 And Livre.IdExemplaire = Emprun.FkExemplaire
 And Emprun.FkAdherant = Adherant.IdAdherant
-And Adherant.IdAdherant = ???
+And Adherant.IdAdherant = '???'
 And Emprun.DateRetour IS NULL;
+/*	*/
+
 
 /* reservation */
 Select * 
 From Reservation, Oeuvre, Adherant 
 Where Oeuvre.IdLivre = Reservation.FkLivre
 And Reservation.FkAdherant = Adherant.IdAdherant
-And Adherant.IdAdherant = ???
+And Adherant.IdAdherant = '???'
 And Reservation.DateAcceptation is null;
+/*	*/
+
 
 /* Requete/demande */
+/* Visualisation */ 
 Select * 
 From Requete, Oeuvre, Adherant 
 Where Oeuvre.IdLivre = Requete.FkLivre
 And Requete.FkAdherant = Adherant.IdAdherant
-And Adherant.IdAdherant = ???;
+And Adherant.IdAdherant = '???';
+/* Demande */ 
+INSERT INTO Renouvelement(FkAdherant, FkLivre, Requete)
+VALUES ('???', '???', '???');
+/*	*/
+
+/* Renouvelement */ 
+/* Demande */
+INSERT INTO Renouvelement(FkEmprun, DateDemande)
+VALUES ('???', '???');
+/*	*/
 
 /* Contact */
- 
 Select * from Admin; 
+/*	*/
+/*	*/
+
+
+/* Admin */
+/* Visualisé les tables  */
+Select * from '???'; 
+/*	*/
+
+/* Ajoue valeur */
+INSERT INTO '???'('???', '???', '???')
+VALUES ('???', '???', '???');
+/*	*/
+
+
+
+
+
+/*	*/
